053e8924a8d89f5635ecb25029bffd9599810647
akhila2711 <akhilakhi112799@gmail.com>
2025-05-05 13:31:23 +0530

    add

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+// import Header from './component/Header/Header'
+import LifeCycle from './component/Header/lifeCycle';
+
+function App() {
+  return (
+<>
+{/* <Header></Header> */}
+<LifeCycle></LifeCycle>
+</>
+  );
+}
+
+export default App;
